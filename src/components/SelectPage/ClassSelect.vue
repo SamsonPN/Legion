bfbb86182975c3a15303da0fb0032c77b328ee8b
Samson <samsonn9900@gmail.com>
2020-01-16 17:48:39 -0800

    Working on adding effects to Class Select page

--- src/components/SelectPage/ClassSelect.vue
+++ src/components/SelectPage/ClassSelect.vue
@@ -1,19 +1,11 @@
 <template>
   <div :id="archetype + 'Select'" class="ClassSelect">
       <p>{{archetype}}</p>
-      <div v-for="(_class, key) in classList" :key="key">
-          <p>{{key}}</p>
+      <div v-for="(_class, className) in classList" :key="className">
+          <p>{{className}}</p>
+
           <textarea
-            :id="key"
-            :value="_class.name"
-            @change="ignChange"
-            @keypress="isAlphaNum"
-            placeholder="IGN"
-            maxLength="12"
-            rows="1">
-          </textarea>
-          <textarea
-            :id="key"
+            :id="className"
             class="levelTextArea"
             placeholder="Level"
             :value="_class.level"
@@ -22,6 +14,8 @@
             maxLength="3"
             rows="1">
           </textarea>
+
+          <p>{{ effect(className) }}</p>
       </div>
   </div>
 </template>
@@ -30,6 +24,7 @@
 import { mapGetters, mapMutations } from 'vuex';
 import Ranks from './ClassSelectRanks';
 import characterCardMixin from '../../mixins/characterCardMixin';
+import CharacterEffect from '../Characters/CharacterEffect';
 
 export default {
     name: "Select",
@@ -47,31 +42,11 @@ export default {
                 e.preventDefault();
             }
         },
-        isAlphaNum(e){
-            let alphnum = /\w/;
-            let match = e.key.match(alphnum) ? true : false;
-            if (! match) {
-                alert('Letters/Numbers only!');
-                e.preventDefault();
-            }
-            if(e.key === 'Enter'){
-                e.preventDefault();
-            }
-        },
-        ignChange(e){
-            let {id, value} = e.target;
-            let data = {
-                field: 'name',
-                value,
-                archetype: this.archetype,
-                className: id
-            }
-            this.updateCharData(data)
-        },
         levelChange(e){
             let {id, value} = e.target;
             let {archetype} = this;
             value = parseInt(value);
+            // just call checkNumbersOnly
             if(isNaN(value)){
                 alert('Please enter numbers only!');
                 e.target.value = '0';
@@ -96,8 +71,13 @@ export default {
                 coordinates
             };
             this.updateCharData(data);
+        },
+        effect(charName){
+            let {classes, effects} = CharacterEffect;
+            let effectNum = classes[charName];
+            return effects[effectNum].effect;
         }
-    },
+    }
 }
 </script>
 
